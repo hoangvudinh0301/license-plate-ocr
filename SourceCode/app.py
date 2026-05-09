a8a50bf3374be13be43faf211e5a1da9d631c59f
Hoang Vu Dinh <hoangvudinh31@gmail.com>
2026-05-09 22:11:50 +0700

    add app.py file

---
+++ SourceCode/app.py
@@ -0,0 +1,30 @@
+import gradio as gr
+import cv2
+import numpy as np
+
+from img import process_image
+from cam import process_video
+
+image_interface = gr.Interface(
+    fn=process_image,
+    inputs=gr.Image(type="numpy"),
+    outputs=[
+        gr.Image(type="numpy"),
+        gr.Textbox(label="Plate Number")
+    ],
+    title="Vietnam License Plate Recognition",
+    description="Upload vehicle image"
+)
+
+video_interface = gr.Interface(
+    fn=process_video,
+    inputs=gr.Video(),
+    outputs=gr.Video(),
+    title="Video License Plate Recobnition"
+)
+
+demo = gr.TabbedInterface(
+    [image_interface, video_interface],
+    ["Imgae", "Video"]
+)
+demo.launch()
